c6b2f61a60d4d8b63ce0ca3881fe4464375c2584
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-13 19:13:01 -0400

    Commit owoify.js

--- modules/cmds.js
+++ modules/cmds.js
@@ -1,12 +1,12 @@
-const checks = require("./cmds/checks")
 // command modules
 const ping = require("./cmds/ping")
 const help = require("./cmds/help")
 const bible = require("./cmds/bible");
 const bm = require("./cmds/bm")
 const xkcd = require("./cmds/xkcd");
+const owoify = require("./cmds/owoify")
 
-generateCommands = (app, config) => [ping, bible, bm, xkcd, {
+generateCommands = (app, config) => [ping, bible, bm, owoify, xkcd, {
     "name": ["help", "h", "command", "cmd", "commands", "cmds"],
     "desc": "Provide command information",
     "desc_ext": "Accepts a command as a parameter",
---
+++ modules/cmds/owoify.js
@@ -0,0 +1,18 @@
+const faces = ["(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^"];
+owoify = string => {
+    string = string.replace(/(?:r|l)/g, "w");
+    string = string.replace(/(?:R|L)/g, "W");
+    string = string.replace(/n([aeiou])/g, 'ny$1');
+    string = string.replace(/N([aeiou])/g, 'Ny$1');
+    string = string.replace(/N([AEIOU])/g, 'Ny$1');
+    string = string.replace(/ove/g, "uv");
+    string = string.replace(/\!+/g, " " + faces[Math.floor(Math.random() * faces.length)] + " ");
+    return string;
+}
+const checks = require("./checks")
+module.exports = {
+    "name": ["owoify", "owo", "o"],
+    "desc": "Return an owoified message",
+    "func": (msg, args) => msg.channel.send(owoify(args.join(" "))),
+    "checks": [checks.argsExist]
+}
