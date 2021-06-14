29f7ff91828be5a2f0f593829a9f2e33dc156378
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-13 23:50:28 -0400

    Add bible_verse.random command

--- modules/cmds/bible.js
+++ modules/cmds/bible.js
@@ -1,20 +1,34 @@
 const {
-    argsExist
+    cleanArgsExist
 } = require("./checks");
 const fetch = require("node-fetch");
 
+const url = "https://bible-api.com/";
+const urlRandom = "https://labs.bible.org/api/?passage=random&type=json";
+
 get_verse = async (reference = "John 3:16", translation = "KJV") => {
-    var url = "https://bible-api.com/";
-    const response = await fetch(`${url}${reference}?translation=${translation}`);
-    const data = await response.json();
-    if (data.error) return { error: data.error };
-    else if (typeof data == "string") return { error: data };
+    var resp = await fetch(`${url}${reference}?translation=${translation}`);
+    resp = await resp.json();
+    if (resp.error) return {
+        error: resp.error
+    };
+    else if (typeof resp == "string") return {
+        error: resp
+    };
     else return {
-        url: `https://www.biblegateway.com/passage/?search=${data.reference.replace(/ /g, "%20")}`,
-        ref: data.reference,
-        text: data.text,
-        translation: data.translation_name
+        url: `https://www.biblegateway.com/passage/?search=${resp.reference.replace(/ /g, "%20")}`,
+        ref: resp.reference,
+        text: resp.text,
+        translation: resp.translation_name
     }
+}
+
+getRandomVerse = async (translation = "KJV") => {
+    var resp = await fetch(urlRandom);
+    resp = (await resp.json())[0];
+    var reference = `${resp.bookname} ${resp.chapter}:${resp.verse}`;
+    resp = await get_verse(reference, translation)
+    return resp
 }
 
 embedConstructor = resp => {
@@ -40,7 +54,7 @@ module.exports = {
             "Clementine (Latin), Almeida (Portuguese), or RCCV (Romanian)",
         ].join("\n    ")
     ],
-    "checks": argsExist,
+    "checks": cleanArgsExist,
     "func": (msg, args) => {
         var translation = "KJV";
         if (msg.tags.t) {
@@ -49,16 +63,46 @@ module.exports = {
             translation = String(msg.tags.translation);
         };
 
-        get_verse(args.join(" "), translation).then(text => {
-            if (text.error) {
-                msg.channel.send(`error: ${text.error}`); return;
-            } else if (text.text.length > 1000) {
-                msg.channel.send(`error: too long\nthis link might work: <${text.url}>`); return;
+        get_verse(args.join(" "), translation).then(resp => {
+            if (resp.error) {
+                msg.channel.send(`error: ${resp.error}`);
+                return;
+            } else if (resp.text.length > 1000) {
+                msg.channel.send(`error: too long\nthis link might work: <${resp.url}>`);
+                return;
             };
 
             msg.channel.send({
-                embed: embedConstructor(text)
+                embed: embedConstructor(resp)
             })
         });
-    }
+    },
+    cmds: [{
+        "name": ["random", "r"],
+        "desc": "Return a random bible verse",
+        "desc_ext": "Although the same language options are available as the normal command, issues are *very* likely to occur with non-English selections",
+        "func": (msg, args) => {
+            var translation = "KJV";
+            if (msg.tags.t) {
+                translation = String(msg.tags.t);
+            } else if (msg.tags.translation) {
+                translation = String(msg.tags.translation);
+            };
+
+            getRandomVerse(translation)
+                .then(resp => {
+                    if (resp.error) {
+                        msg.channel.send(`error: ${resp.error}`);
+                        return;
+                    } else if (resp.text.length > 1000) {
+                        msg.channel.send(`error: too long\nthis link might work: <${resp.url}>`);
+                        return;
+                    };
+
+                    msg.channel.send({
+                        embed: embedConstructor(resp)
+                    })
+                })
+        }
+    }]
 }
