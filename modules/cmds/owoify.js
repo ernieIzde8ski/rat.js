1fd8f9446e8be36afc5b4de1280aa87404f64abf
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-07-25 04:25:12 -0400

    Initial deletions

--- modules/cmds/owoify.js
+++
@@ -1,9 +0,0 @@
-const { argsExist } = require("./checks");
-const owoify = require("../functions/owoify");
-
-module.exports = {
-    "name": ["owoify", "owo", "o"],
-    "desc": "Return an owoified message",
-    "func": (msg, args) => msg.channel.send(owoify(args.join(" "))),
-    "checks": argsExist
-};
