bed1ca4d098aa4eb480f2353bbf41d5531fe4c3d
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-08-28 20:43:25 -0400

    Commit Context constructor

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
+import * as configs from "./configs.json";
 import * as token from "./token.json";
-import { Client } from "@typeit/discord";
+
+import { Context } from "./modules/context";
 import { Message } from "discord.js";
+import { Client } from "@typeit/discord";
+
 const client = new Client({
     classes: [
         `${__dirname}/*Discord.ts`,
@@ -14,10 +18,15 @@ client.once("ready", () => { console.log(client.user.tag + " is Online!") })
 client.on("message", async (message: Message) => {
 
     if (message.channel.type === "text" || message.channel.type === "news")
-        if (message.channel.name === "krysa" && message.content !== "krysa")
+        if (message.channel.name === configs.trigger && message.content !== configs.trigger)
             try { return await message.delete() } catch (e) { return };
     if (message.author.bot) return;
-    if (message.content.toLowerCase().includes("krysa")) message.channel.send("krysa");
+    if (message.content.split(" ").includes(configs.trigger)) message.channel.send(configs.trigger);
+
+    if (message.content.startsWith(configs.prefix)) {
+        let context = new Context(message, configs.prefix);
+        message.channel.send("Context: \n```" + context.toString() + "\n```")
+    };
 })
 
 async function start() {
---
+++ modules/context.ts
@@ -0,0 +1,42 @@
+import { Message } from "discord.js";
+
+export class Context {
+    message: Message;
+    invoked_with: string;
+    command: string | undefined;
+    arguments: string[];
+    flags: any;
+
+
+    constructor(message: Message, prefix: string) {
+        this.message = message;
+        this.invoked_with = prefix;
+
+        let __args__: string;
+        let __flags__: string[];
+        [__args__, ...__flags__] = this.message.content.slice(this.invoked_with.length).split(/\s*--/);
+        this.arguments = __args__.split(" ");
+        this.command = this.arguments.shift();
+
+        this.flags = {};
+        for (var i of __flags__) {
+            let [key, ...values] = i.split(/\s+/);
+            let value = values.join(" ");
+
+            if (value === "") this.flags[key] = true;
+            else {
+                try {
+                    this.flags[key] = JSON.parse(value);
+                } catch (e) {
+                    this.flags[key] = value;
+                }
+            }
+        }
+
+    }
+
+    toString(): string {
+        // console.log(this)
+        return `Message ID: ${this.message.id}\nPrefix: ${this.invoked_with}\nCommand: ${this.command}\nArguments: ${JSON.stringify(arguments)}\nFlags: ${JSON.stringify(this.flags)}`;
+    }
+}
