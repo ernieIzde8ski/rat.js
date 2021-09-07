1347462d67400bd80f7202e7dff546fb911b793f
Ernie <44341687+ernieIzde8ski@users.noreply.github.com>
2021-09-07 12:40:19 -0400

    Commit randomized.ts from chromebook

---
+++ modules/commands/randomized.ts
@@ -0,0 +1,17 @@
+import { Bot, Context } from "../commands";
+
+function random_choice(arr: Array<any>): any {
+    if (!arr.length) throw new Error();
+    return arr[Math.floor(arr.length * Math.random)];
+}
+
+module.exports = {
+    cmds: [{
+        name: "random_choice", aliases: ["random", "pick"],
+        desc: "Return a random selection from an array",
+        func: async (bot: Bot, ctx: Context) => {
+            const args = ctx.args.join(" ").split(/,\s*|\s+/gm);
+            await ctx.send(random_choice(args));
+        }
+    }]
+} 
