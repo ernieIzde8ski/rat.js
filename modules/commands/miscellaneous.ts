7f857cd181e92a8cc7ccb8a15ce3d19bf3de2f1a
Ernie <44341687+ernieIzde8ski@users.noreply.github.com>
2021-09-07 12:53:04 -0400

    Commit incomplete miscellaneous.ts

---
+++ modules/commands/miscellaneous.ts
@@ -0,0 +1,9 @@
+import { Bot, Context } from "../commands";
+
+module.exports = {cmds: [{
+  name: "birana", desc: "Returns armenium birana status",
+  func: async (bot: Bot, ctx: Context) => {
+    const args = ctx.args.join(" ");
+    const armenium = ();
+}
+}]}
