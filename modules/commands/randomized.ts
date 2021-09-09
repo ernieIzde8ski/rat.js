4dd713cdfef70779e7f33338248181c53d238137
Ernie <44341687+ernieIzde8ski@users.noreply.github.com>
2021-09-09 10:08:16 -0400

    Commit random_song command

--- modules/commands/randomized.ts
+++ modules/commands/randomized.ts
@@ -4,6 +4,10 @@ function random_choice(arr: Array<any>): any {
     if (!arr.length) throw new Error();
     return arr[Math.floor(arr.length * Math.random())];
 }
+function random_song(arr: Array<string>): string {
+    if (!arr?.length) return "7-iRf9AWoyE";
+    return arr[Math.floor(arr.length * Math.random())];
+}
 
 module.exports = {
     cmds: [{
@@ -12,6 +16,12 @@ module.exports = {
         func: async (bot: Bot, ctx: Context) => {
             const args = ctx.args.join(" ").split(/,\s*/gm);
             await ctx.send(random_choice(args));
+        }
+    }, {
+        name: "random_song", aliases: ["rs", "song"],
+        desc: "Return a random song",
+        func: async (bot: Bot, ctx: Context) => {
+            await ctx.send(`https://youtu.be/${random_song(bot.configs.songs)}`
         }
     }]
 } 
