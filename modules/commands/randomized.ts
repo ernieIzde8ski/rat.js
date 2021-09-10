3cfc8367905120f5623da6fa66827414ec434822
Ernie <44341687+ernieIzde8ski@users.noreply.github.com>
2021-09-10 11:50:51 -0400

    Commit initial bm command

--- modules/commands/randomized.ts
+++ modules/commands/randomized.ts
@@ -1,4 +1,6 @@
 import { Bot, Context } from "../commands";
+const seedrandom = require("seedrandom");
+
 
 function random_choice(arr: Array<any>): any {
     if (!arr.length) throw new Error();
@@ -22,6 +24,14 @@ module.exports = {
         desc: "Return a random song",
         func: async (bot: Bot, ctx: Context) => {
             await ctx.send(`https://youtu.be/${random_song(bot.configs.songs)}`
+        }
+    }, {
+        name: "based_meter", aliases: ["bm"],
+        desc: "Determine basedness",
+        func: async (bot: Bot, ctx: Context) => {
+            let args = ctx.args.join(' ');
+            if (!args.length) args = "Your";
+            
         }
     }]
 } 
