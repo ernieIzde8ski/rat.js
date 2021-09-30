c881dadc0ddebcd4867e8f866448c3b27ee9171a
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-09-30 05:59:54 -0400

    Use not_enough_bibles module

--- modules/commands/bible.ts
+++ modules/commands/bible.ts
@@ -1,39 +1,17 @@
 import { Client, Context } from "../commands";
-import axios from "axios";
-import { TranslationNotFound } from "../errors";
-const twitterSplitter: ((text: string, limit: number, joiner: string) => string[]) = require("twitter-splitter");
-
-
-type Verse = { book_id: string, book_name: string, chapter: number, verse: number, text: string };
-type BibleResp = { reference: string, verses: Array<Verse>, text: string, translation_id: string, translation_name: string, translation_note: string };
-type BibleResps = BibleResp | { error: string } | "translation not found";
-type TextResp = { ref: string, text: string, translation: string };
-
-
-const regex = /[&/?=]/gm
-async function get_text(ref: string, translation: string = "kjv", characters_per_line: number = 100, display_verse: boolean = true): Promise<TextResp> {
-    [ref, translation] = [ref.replace(regex, ""), translation.replace(regex, "")];
-    const link = `https://bible-api.com/${ref}?translation=${translation}`;
-    const data: BibleResps = (await axios.get(link)).data;
-    if (typeof data === "string") throw new TranslationNotFound(translation);
-    else if ("error" in data) throw new Error(data.error);
-
-    if (!display_verse) var text = data.text.trim().replace(/\s+/g, " ");
-    else var text = data.verses.map(verse => `${verse.verse} ` + verse.text.trim().replace(/\s+/g, " ")).join(" ");
-
-    return { ref: data.reference, text: twitterSplitter(text, characters_per_line, "").join("\n"), translation }
-}
+import { get_passage } from "not_enough_bibles";
 
 
 module.exports = {
     cmds: [{
         name: "verse", aliases: ["text", "v"],
         func: async (bot: Client, ctx: Context) => {
-            const resp = await get_text(ctx.args.join(" "), ctx.flags.translation ?? "kjv", (ctx.flags.characters ?? ctx.flags.characters_per_line) ?? 50, ctx.flags.display_verse ?? true)
+            const options = { translation: ctx.flags.translation ?? "KJV", processText: true, maxChars: ctx.flags.chars ?? 50 }
+            const resp = await get_passage(ctx.args.join(" "), options);
             const embed = {
-                title: resp.ref, description: "```\n" + resp.text + "\n```", color: ctx.self.displayColor,
-                url: `https://www.biblegateway.com/passage/?search=${resp.ref.replace(" ", "%20")}&version=NIV`,
-                footer: { text: `Translation: ${resp.translation}` }
+                title: resp.reference, description: "```\n" + resp.text + "\n```", color: ctx.self.displayColor,
+                url: `https://www.biblegateway.com/passage/?search=${resp.reference.replace(" ", "%20")}&version=NIV`,
+                footer: { text: `Translation: ${resp.translation_id}` }
             }
             await ctx.send({ embed: embed })
         }
